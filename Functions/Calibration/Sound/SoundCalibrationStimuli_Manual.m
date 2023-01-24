cf70ff32abc154b97237efe0f379e6844f3ab29c
abakutyte <algebak@gmail.com>
2023-01-24 11:29:02 +0000

    Calibration done on both speakers at the same time
    Needs to be tested on setup

--- Functions/Calibration/Sound/SoundCalibrationStimuli_Manual.m
+++ Functions/Calibration/Sound/SoundCalibrationStimuli_Manual.m
@@ -25,19 +25,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % volumeRange is a 2-element vector specifying the range of volume to test
 % nValueToTest = number of value within volumeRange. These will be log-spaced.
 % dbSPL_Target is the target sound level for each frequency in dB SPL
-% nSpeakers is 1 for mono and 2 for stereo
 %
 % Example:
-% SoundCal = SoundCalibrationStimuli_Manual([40 80], 10, 60, 2); 
-% 1 - Find attenuation factors for taget value (60 dB) for noise and then signal+noise
-% for each speaker.
+% SoundCal = SoundCalibrationStimuli_Manual([40 80], 10, 60); 
+% 1 - Find attenuation factors for target value (60 dB) for noise and then signal+noise
 % 2 - Test volume outputted using attenuation factor within volume range set 
 % (40dB to 80dB) for nValueToTest (10). 
 % 3 - Save calibration file in default location (BpodLocal folder).
 %
 
 
-function SoundCal = SoundCalibrationStimuli_Manual(volumeRange,nValueToTest, dbSPL_Target, nSpeakers)
+% function SoundCal = SoundCalibrationStimuli_Manual(volumeRange,nValueToTest, dbSPL_Target, nSpeakers)
+function SoundCal = SoundCalibrationStimuli_Manual(volumeRange,nValueToTest, dbSPL_Target)
 
 global BpodSystem
 %% Resolve HiFi Module USB port
@@ -63,59 +62,48 @@ AcceptableDifference_dBSPL = 0.5;
 if (dbSPL_Target < 10) || (dbSPL_Target > 120)
     error('Error: target dB SPL must be in range [10, 120]')
 end
-if nSpeakers > 2
-    error('Error: this function can calibrate 1 or 2 speakers.')
-end
 
 % Setup struct
 SoundCal = struct;
-for i = 1:nSpeakers
-    SoundCal(i).Table = [];
-    SoundCal(i).Sounds = Sounds.Stream;
-    SoundCal(i).Name = Sounds.Name;
-    SoundCal(i).TargetSPL = dbSPL_Target;
-    SoundCal(i).LastDateModified = date;
-    SoundCal(i).Coefficient = [];
-end
+SoundCal.Table = [];
+SoundCal.Sounds = Sounds.Stream;
+SoundCal.Name = Sounds.Name;
+SoundCal.TargetSPL = dbSPL_Target;
+SoundCal.LastDateModified = date;
+SoundCal.Coefficient = [];
 
 %% Calibration loop
-speakerNames = {'Left', 'Right'};
-for s = 1:nSpeakers
-    disp([char(10) 'Begin calibrating ' speakerNames{s} ' speaker.'])
-    for n = 1:length(Sounds.Name) 
-        attFactor = 0.2;
-        found = 0;
-        nTries = 0;
-        while found == 0
-            nTries = nTries + 1;
-            if nTries > nTriesPerSound
-                error(['Error: Could not resolve an attenuation factor for ' Sounds.Name{n}])
-            end
-            input([num2str(nTries) ' - Press Enter to play the next sound (' num2str(n) '/' num2str(length(Sounds.Name)) '). This tone = ' Sounds.Name{n} ' , ' num2str(attFactor) ' FS amplitude.'], 's'); 
-            sound = Sounds.Stream{n} * attFactor;
-            if s == 1
-                H.load(1, [sound; zeros(1,length(sound))], 'LoopMode', 1, 'LoopDuration', soundDuration); 
-                H.push; pause(.1);
-            else
-                H.load(1, [zeros(1,length(sound)); sound], 'LoopMode', 1, 'LoopDuration', soundDuration); 
-                H.push; pause(.1);
-            end
-            
-            H.play(1);
-            pause(soundDuration);
-            dbSPL_Measured = input(['Enter dB SPL measured > ']);
-            if abs(dbSPL_Measured - dbSPL_Target) <= AcceptableDifference_dBSPL
-                SoundCal(s).AttenuationFactor{n} = attFactor;
-                found = 1;
-            else
-                AmpFactor = sqrt(10^((dbSPL_Measured - dbSPL_Target)/10));
-                attFactor = attFactor/AmpFactor;
-                attFactor2 = input(['Next attenuation factor tested:' num2str(attFactor) '. Alternative attenuation factor to test:' num2str(attFactor) '? ']);
-                if attFactor2>0; attFactor=attFactor2; end; clear attFactor2 P yfit
-            end
+
+disp([char(10) 'Begin calibrating both speaker.'])
+for n = 1:length(Sounds.Name) 
+    attFactor = 0.2;
+    found = 0;
+    nTries = 0;
+    while found == 0
+        nTries = nTries + 1;
+        if nTries > nTriesPerSound
+            error(['Error: Could not resolve an attenuation factor for ' Sounds.Name{n}])
+        end
+        input([num2str(nTries) ' - Press Enter to play the next sound (' num2str(n) '/' num2str(length(Sounds.Name)) '). This tone = ' Sounds.Name{n} ' , ' num2str(attFactor) ' FS amplitude.'], 's'); 
+        sound = Sounds.Stream{n} * attFactor;
+        % load the sound to hifi module
+        H.load(1, [sound; sound], 'LoopMode', 1, 'LoopDuration', soundDuration); 
+        H.push; pause(.1);
+        H.play(1);
+        pause(soundDuration);
+        dbSPL_Measured = input(['Enter dB SPL measured > ']);
+        if abs(dbSPL_Measured - dbSPL_Target) <= AcceptableDifference_dBSPL
+            SoundCal.AttenuationFactor{n} = attFactor;
+            found = 1;
+        else
+            AmpFactor = sqrt(10^((dbSPL_Measured - dbSPL_Target)/10));
+            attFactor = attFactor/AmpFactor;
+            attFactor2 = input(['Next attenuation factor tested:' num2str(attFactor) '. Alternative attenuation factor to test:' num2str(attFactor) '? ']);
+            if attFactor2>0; attFactor=attFactor2; end; clear attFactor2 P yfit
         end
     end
 end
+
 
 %% Save sound calibration file
 save([pathToCalibFiles '/SoundCalibrationOngoing.mat'] , 'SoundCal')
@@ -125,55 +113,48 @@ save([pathToCalibFiles '/SoundCalibrationOngoing.mat'] , 'SoundCal')
 minVol = volumeRange(1);
 maxVol = volumeRange(2);
 volumeVector =  logspace(log10(minVol),log10(maxVol),nValueToTest);
-speakerNames = {'Left', 'Right'};
-for s = 1:nSpeakers
-    disp([char(10) 'Begin testing ' speakerNames{s} ' speaker.'])
-    for n = 1:length(Sounds.Name) 
-        ThisTable = zeros(nValueToTest, 2);
-        for v = 1:nValueToTest
-            attFactor = SoundCal(s).AttenuationFactor{n};
-            % estimate theoritical att factor for volume to test based on targetSPL att factor
-            diffSPL = volumeVector(v) - [SoundCal(1,s).TargetSPL];
-            diffFactor = sqrt(10.^(diffSPL./10)); 
-            attFactor = attFactor*diffFactor;                 
-            found = 0;
-            nTries = 0;
-            while found == 0
-                nTries = nTries + 1;
-                if nTries > nTriesPerSound
-                    error(['Error: Could not resolve an attenuation factor for ' num2str(volumeVector(v))])
-                end
-                sound = Sounds.Stream{n} * attFactor;
-                input([num2str(nTries) ' - Press Enter to play the next tone (' num2str(v) '/' num2str(nValueToTest) '). ' Sounds.Name{n} ' : ' num2str(volumeVector(v)) ' Hz, ' num2str(attFactor) ' FS amplitude.'], 's'); 
-                if s == 1
-                    H.load(1, [sound; zeros(1,length(sound))], 'LoopMode', 1, 'LoopDuration', soundDuration); 
-                    H.push; pause(.1);
-                else
-                    H.load(1, [zeros(1,length(sound)); sound], 'LoopMode', 1, 'LoopDuration', soundDuration); 
-                    H.push; pause(.1);
-                end
-                H.play(1);
-                pause(soundDuration);
-                dbSPL_Measured = input(['Enter dB SPL measured > ']);              
-                if abs(dbSPL_Measured - volumeVector(v)) <= AcceptableDifference_dBSPL % save attFactor in table
-                    ThisTable(v,1) = volumeVector(v);
-                    ThisTable(v,2) = attFactor;
-                    found = 1;
-                else % compute new att factor to test
-                    AmpFactor = sqrt(10^((dbSPL_Measured - volumeVector(v))/10)); 
-                    attFactor = attFactor/AmpFactor;
-                    attFactor2 = input(['Next attenuation factor tested:' num2str(attFactor) '. Alternative attenuation factor to test:' num2str(attFactor) '? ']);
-                    if attFactor2>0; attFactor=attFactor2; end; clear attFactor2 P yfit
-                end
-            end % each attempt to find att factor
-        end % each volume value tested
-        % add att factors to table for each sound
-        ThisTable = [0 0;ThisTable]; % add value zero so that calibration curve goes through zero
-        SoundCal(s).Table{n} = ThisTable;
-        SoundCal(s).Coefficient{n} = glmfit(ThisTable(:,1),ThisTable(:,2),'binomial');
-        save([pathToCalibFiles 'SoundCalibrationOngoing.mat'] , 'SoundCal')
-    end % each sound
-end % each speaker
+
+disp([char(10) 'Begin testing both speakers.'])
+for n = 1:length(Sounds.Name) 
+    ThisTable = zeros(nValueToTest, 2);
+    for v = 1:nValueToTest
+        attFactor = SoundCal.AttenuationFactor{n};
+        % estimate theoritical att factor for volume to test based on targetSPL att factor
+        diffSPL = volumeVector(v) - [SoundCal.TargetSPL];
+        diffFactor = sqrt(10.^(diffSPL./10)); 
+        attFactor = attFactor*diffFactor;                 
+        found = 0;
+        nTries = 0;
+        while found == 0
+            nTries = nTries + 1;
+            if nTries > nTriesPerSound
+                error(['Error: Could not resolve an attenuation factor for ' num2str(volumeVector(v))])
+            end
+            sound = Sounds.Stream{n} * attFactor;
+            input([num2str(nTries) ' - Press Enter to play the next tone (' num2str(v) '/' num2str(nValueToTest) '). ' Sounds.Name{n} ' : ' num2str(volumeVector(v)) ' Hz, ' num2str(attFactor) ' FS amplitude.'], 's'); 
+            H.load(1, [sound; sound], 'LoopMode', 1, 'LoopDuration', soundDuration); 
+            H.push; pause(.1);
+            H.play(1);
+            pause(soundDuration);
+            dbSPL_Measured = input(['Enter dB SPL measured > ']);              
+            if abs(dbSPL_Measured - volumeVector(v)) <= AcceptableDifference_dBSPL % save attFactor in table
+                ThisTable(v,1) = volumeVector(v);
+                ThisTable(v,2) = attFactor;
+                found = 1;
+            else % compute new att factor to test
+                AmpFactor = sqrt(10^((dbSPL_Measured - volumeVector(v))/10)); 
+                attFactor = attFactor/AmpFactor;
+                attFactor2 = input(['Next attenuation factor tested:' num2str(attFactor) '. Alternative attenuation factor to test:' num2str(attFactor) '? ']);
+                if attFactor2>0; attFactor=attFactor2; end; clear attFactor2 P yfit
+            end
+        end % each attempt to find att factor
+    end % each volume value tested
+    % add att factors to table for each sound
+    ThisTable = [0 0;ThisTable]; % add value zero so that calibration curve goes through zero
+    SoundCal.Table{n} = ThisTable;
+    SoundCal.Coefficient{n} = glmfit(ThisTable(:,1),ThisTable(:,2),'binomial');
+    save([pathToCalibFiles 'SoundCalibrationOngoing.mat'] , 'SoundCal')
+end % each sound
 
 answer = questdlg('Successful sound calibration! Replace old calibration file','Yes');
 % replace old calibration file and delete temp calibration file
